b13be4ffb101ec27437a36959893269abb5f02c3
Williamsoditi <williamsoditi99@gmail.com>
2024-03-14 14:52:37 +0300

    Feat : Adds loop to display languages and their codes to console

--- js/main.js
+++ js/main.js
@@ -2,5 +2,23 @@ const fromText = document.querySelector(".from-text"),
     toText = document.querySelector(".to-text"),
     selectTag = document.querySelectorAll("select"),
     exchange = document.querySelector(".exchange"),
-    translate = document.querySelector("button"),
+    translate = document.querySelector("button")
     icons = document.querySelector("i")
+
+selectTag.forEach((tag,id) => {
+    for(const langCode in languages){
+        //console.log(languages[langCode]);
+        let selected;
+
+        if (id == 0 && langCode == "en-UK") {
+            selected = "selected"
+        }
+        else if (id == 1 && langCode == "tr-TR") {
+            selected = "selected"
+        }
+
+        let option = `<option value = "${langCode}" ${selected}> ${languages[langCode]}</option>`
+
+        tag.insertAdjacentHTML("beforeend", option)
+    }
+});
