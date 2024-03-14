11cf7d03a0d8980512a6d9ffc9a22011b5aefafb
Williamsoditi <williamsoditi99@gmail.com>
2024-03-14 14:02:56 +0300

    Feat : Adds variables to select queries

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const fromText = document.querySelector(".from-text"),
+    toText = document.querySelector(".to-text"),
+    selectTag = document.querySelectorAll("select"),
+    exchange = document.querySelector(".exchange"),
+    translate = document.querySelector("button"),
+    icons = document.querySelector("i")
